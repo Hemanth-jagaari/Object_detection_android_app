ae9fe291dacfc8556f8d702b0c957d403e29eb90
hemanth <hemanthjagaari@gmail.com>
2021-06-04 22:11:57 +0530

    review_2

--- app/src/main/java/com/example/first_version/Draw.java
+++ app/src/main/java/com/example/first_version/Draw.java
@@ -24,10 +24,10 @@ public class Draw extends View {
         this.rect=rect;
         this.text=label;
         paint.setColor(Color.RED);
-        paint.setStrokeWidth(10f);
+        paint.setStrokeWidth(5f);
         paint.setStyle(Paint.Style.STROKE);
         textpaint=new Paint();
-        textpaint.setColor(Color.RED);
+        textpaint.setColor(Color.GREEN);
         textpaint.setStyle(Paint.Style.FILL);
         textpaint.setTextSize(80f);
     }
@@ -37,5 +37,6 @@ public class Draw extends View {
         super.onDraw(canvas);
         canvas.drawText(text,rect.centerX(),rect.centerY(),textpaint);
         canvas.drawRect(rect.left,rect.top,rect.right,rect.bottom,paint);
+        //canvas.drawText(text,rect.left,rect.right,textpaint);
     }
 }
--- app/src/main/java/com/example/first_version/MainActivity2.java
+++ app/src/main/java/com/example/first_version/MainActivity2.java
@@ -21,6 +21,7 @@ import android.graphics.RectF;
 import android.graphics.YuvImage;
 import android.media.Image;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.text.TextUtils;
 import android.util.Log;
 import android.util.Size;
@@ -37,8 +38,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import android.graphics.Matrix;
+import android.widget.Toast;
+
+import static androidx.core.math.MathUtils.clamp;
 
 public class MainActivity2 extends AppCompatActivity {
     private PreviewView previewview;
@@ -46,7 +51,7 @@ public class MainActivity2 extends AppCompatActivity {
     private ObjectDetector.ObjectDetectorOptions options;
     private ObjectDetector objectDetector;
     private ActivityMain2Binding binding;
-
+    private TextToSpeech textToSpeech;
 
 
     @Override
@@ -56,7 +61,25 @@ public class MainActivity2 extends AppCompatActivity {
 
         previewview = findViewById(R.id.previewview);
         Log.d("Test:","previewFIND");
-        options = ObjectDetector.ObjectDetectorOptions.builder().setMaxResults(4).build();
+        options = ObjectDetector.ObjectDetectorOptions.builder().setMaxResults(2).build();
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int ttsLang = textToSpeech.setLanguage(Locale.US);
+
+                    if (ttsLang == TextToSpeech.LANG_MISSING_DATA
+                            || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "The Language is not supported!");
+                    } else {
+                        Log.i("TTS", "Language Supported.");
+                    }
+                    Log.i("TTS", "Initialization success.");
+                } else {
+                    Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
         try {
             objectDetector = ObjectDetector.createFromFileAndOptions(this, "lite-model_metadata_2.tflite", options);
             Log.d("Test:","ObjectDetector Initialized");
@@ -97,18 +120,24 @@ public class MainActivity2 extends AppCompatActivity {
                 @SuppressLint("UnsafeOptInUsageError") Image mediaImage = image.getImage();
                 int rotation=image.getImageInfo().getRotationDegrees();
 
-                Bitmap bitmap=toBitmap(mediaImage);
+                Bitmap bitmap=yuv420ToBitmap(mediaImage);
                 bitmap=rotateBitmap(bitmap,rotation);
 
 
 
                 List<Detection> results = objectDetector.detect(TensorImage.fromBitmap(bitmap));
                 for(Detection res:results){
+                    Log.d("Score ", String.valueOf(res.getCategories().get(0).getScore()));
                     if(binding.parent.getChildCount()>1) binding.parent.removeViewAt(1);
                     RectF rect = res.getBoundingBox();
                     String label=res.getCategories().get(0).getLabel();
 
                     Log.d("Label:",label);
+                    int speechStatus = textToSpeech.speak(label, TextToSpeech.QUEUE_FLUSH, null);
+
+                    if (speechStatus == TextToSpeech.ERROR) {
+                        Log.e("TTS", "Error in converting Text to Speech!");
+                    }
 
                     Draw element=new Draw(MainActivity2.this,rect,TextUtils.isEmpty(label)?"undefined":label);
 
@@ -200,5 +229,60 @@ public class MainActivity2 extends AppCompatActivity {
         ByteArrayInputStream arrayInputStream = new ByteArrayInputStream(byteArray);
         Bitmap bitmap = BitmapFactory.decodeStream(arrayInputStream);
         return bitmap;
+    }
+    public Bitmap yuv420ToBitmap(Image image) {
+        int imageWidth = image.getWidth();
+        int imageHeight = image.getHeight();
+        // sRGB array needed by Bitmap static factory method I use below.
+        int[] argbArray = new int[imageWidth * imageHeight];
+        ByteBuffer yBuffer = image.getPlanes()[0].getBuffer();
+        yBuffer.position(0);
+
+        // This is specific to YUV420SP format where U & V planes are interleaved
+        // so you can access them directly from one ByteBuffer. The data is saved as
+        // UVUVUVUVU... for NV12 format and VUVUVUVUV... for NV21 format.
+        //
+        // The alternative way to handle this would be refer U & V as separate
+        // `ByteBuffer`s and then use PixelStride and RowStride to find the right
+        // index of the U or V value per pixel.
+        ByteBuffer uvBuffer = image.getPlanes()[1].getBuffer();
+        uvBuffer.position(0);
+        int r, g, b;
+        int yValue, uValue, vValue;
+
+        for (int y = 0; y < imageHeight - 2; y++) {
+            for (int x = 0; x < imageWidth - 2; x++) {
+                int yIndex = y * imageWidth + x;
+                // Y plane should have positive values belonging to [0...255]
+                yValue = (yBuffer.get(yIndex) & 0xff);
+
+                int uvx = x / 2;
+                int uvy = y / 2;
+                // Remember UV values are common for four pixel values.
+                // So the actual formula if U & V were in separate plane would be:
+                // `pos (for u or v) = (y / 2) * (width / 2) + (x / 2)`
+                // But since they are in single plane interleaved the position becomes:
+                // `u = 2 * pos`
+                // `v = 2 * pos + 1`, if the image is in NV12 format, else reverse.
+                int uIndex = uvy * imageWidth + 2 * uvx;
+                // ^ Note that here `uvy = y / 2` and `uvx = x / 2`
+                int vIndex = uIndex + 1;
+
+                uValue = (uvBuffer.get(uIndex) & 0xff) - 128;
+                vValue = (uvBuffer.get(vIndex) & 0xff) - 128;
+                r = (int) (yValue + 1.370705f * vValue);
+                g = (int) (yValue - (0.698001f * vValue) - (0.337633f * uValue));
+                b = (int) (yValue + 1.732446f * uValue);
+                r = clamp(r, 0, 255);
+                g = clamp(g, 0, 255);
+                b = clamp(b, 0, 255);
+                // Use 255 for alpha value, no transparency. ARGB values are
+                // positioned in each byte of a single 4 byte integer
+                // [AAAAAAAARRRRRRRRGGGGGGGGBBBBBBBB]
+                argbArray[yIndex] = (255 << 24) | (r & 255) << 16 | (g & 255) << 8 | (b & 255);
+            }
+        }
+
+        return Bitmap.createBitmap(argbArray, imageWidth, imageHeight, Bitmap.Config.ARGB_8888);
     }
 }
